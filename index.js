b2adf5504cf5b29e5df760fc3444c6845db88b23
yaguruma <yaguruma_y@yahoo.co.jp>
2021-03-20 19:03:50 +0900

    first Commit

---
+++ index.js
@@ -0,0 +1,42 @@
+'use strict';
+const http = require('http');
+const server = http
+    .createServer((req, res) => {
+        const now = new Date();
+        console.info(`[${now}] Request by ${req.socket.remoteAddress}`);
+        res.writeHead(200, {
+            'Content-Type': 'text/plain; charset=utf-8',
+        });
+
+        switch (req.method) {
+            case 'GET':
+                res.write(`GET: ${req.url}`);
+                break;
+            case 'POST':
+                res.write(`POST: ${req.url}`);
+                let rawData = '';
+                req.on('data', (chunk) => {
+                    rawData += chunk;
+                }).on('end', () => {
+                    console.info(`[${now}] Data posted: ${rawData}`);
+                });
+                break;
+            case 'DELETE':
+                res.write(`DELETE: ${req.url}`);
+                break;
+            default:
+                break;
+        }
+        res.end();
+    })
+    .on('error', (e) => {
+        console.error(`[${new Date()}] Server Error`, e);
+    })
+    .on('clientError', (e) => {
+        console.error(`[${new Date()}] Client Error`, e);
+    });
+
+const port = 8000;
+server.listen(port, () => {
+    console.info(`[${new Date()}] Listening on ${port}`);
+});
