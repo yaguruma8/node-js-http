e6f28450b92276965a410f6296a8f71709921030
yaguruma <yaguruma_y@yahoo.co.jp>
2021-03-20 22:34:09 +0900

    pugファイルの読み込みの実装

--- index.js
+++ index.js
@@ -1,50 +1,53 @@
 'use strict';
 const http = require('http');
+const pug = require('pug');
 const server = http
-    .createServer((req, res) => {
-        const now = new Date();
-        console.info(`[${now}] Request by ${req.socket.remoteAddress}`);
-        res.writeHead(200, {
-            'Content-Type': 'text/html; charset=utf-8',
-        });
-
-        switch (req.method) {
-            case 'GET':
-                const fs = require('fs');
-                const rs = fs.createReadStream('./form.html');
-                rs.pipe(res);
-                // res.write(`GET: ${req.url}`);
-                break;
-            case 'POST':
-                // res.write(`POST: ${req.url}`);
-                let rawData = '';
-                req.on('data', (chunk) => {
-                    rawData += chunk;
-                }).on('end', () => {
-                    const decoded = decodeURIComponent(rawData);
-                    console.info(`[${now}] Data posted: ${decoded}`);
-                    const qs = require('querystring');
-                    const ans = qs.parse(decoded);
-                    const body = `${ans['name']}さんは${ans['yaki-shabu']}に投票しました。`;
-                    res.write(`<html><body>${body}</body></html>`);
-                    res.end();
-                });
-                break;
-            case 'DELETE':
-                res.write(`DELETE: ${req.url}`);
-                break;
-            default:
-                break;
-        }
-    })
-    .on('error', (e) => {
-        console.error(`[${new Date()}] Server Error`, e);
-    })
-    .on('clientError', (e) => {
-        console.error(`[${new Date()}] Client Error`, e);
+  .createServer((req, res) => {
+    const now = new Date();
+    console.info(`[${now}] Request by ${req.socket.remoteAddress}`);
+    res.writeHead(200, {
+      'Content-Type': 'text/html; charset=utf-8',
     });
+
+    switch (req.method) {
+      case 'GET':
+        // const fs = require('fs');
+        // const rs = fs.createReadStream('./form.html');
+        // rs.pipe(res);
+        res.write(pug.renderFile('./form.pug'));
+        res.end();
+        break;
+      case 'POST':
+        let rawData = '';
+        req
+          .on('data', (chunk) => {
+            rawData += chunk;
+          })
+          .on('end', () => {
+            const decoded = decodeURIComponent(rawData);
+            console.info(`[${now}] Data posted: ${decoded}`);
+            const qs = require('querystring');
+            const ans = qs.parse(decoded);
+            const body = `${ans['name']}さんは${ans['yaki-shabu']}に投票しました。`;
+            res.write(`<html><body>${body}</body></html>`);
+            res.end();
+          });
+        break;
+      case 'DELETE':
+        res.write(`DELETE: ${req.url}`);
+        break;
+      default:
+        break;
+    }
+  })
+  .on('error', (e) => {
+    console.error(`[${new Date()}] Server Error`, e);
+  })
+  .on('clientError', (e) => {
+    console.error(`[${new Date()}] Client Error`, e);
+  });
 
 const port = 8000;
 server.listen(port, () => {
-    console.info(`[${new Date()}] Listening on ${port}`);
+  console.info(`[${new Date()}] Listening on ${port}`);
 });
